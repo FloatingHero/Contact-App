d134ace347dd5b235ec239c3b47c51e814b81ddf
Álvaro <alvaroprezdaz@gmail.com>
2021-08-11 20:21:24 +0100

    added view engine and routes files to project

--- src/index.js
+++ src/index.js
@@ -1,2 +1,41 @@
-//*ARCHIVO PRINCIPAL
-console.log('Archivo de configuración.');
+import express from 'express';
+import dotenv from 'dotenv';
+import path from 'path';
+import hbs from 'express-handlebars';
+import morgan from 'morgan';
+import router from './routes/index';
+
+const app = express();
+dotenv.config();
+
+//*settings
+app.set('port', process.env.APP_PORT);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs',
+    hbs({
+        defaultLayout: 'main',
+        layoutsDir: path.join(app.get('views'), 'layout'),
+        partialsDir: path.join(app.get('views'), 'partials'),
+        extname: '.hbs'
+    }));
+app.set('view engine', '.hbs');
+
+//*middleware
+app.use(morgan('dev'));
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+);
+
+//*static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+//*routes
+app.use(router);
+
+
+//* server listener
+app.listen(app.get('port'), () => {
+    console.log(`Servidor en marcha en el puerto ${app.get('port')}`);
+});
