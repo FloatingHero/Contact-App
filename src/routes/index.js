52f940be1d3b43adba20d58da9337bf4ced3eaaf
Álvaro <alvaroprezdaz@gmail.com>
2021-08-11 21:54:21 +0100

    register form created. Pending to add styles to it.

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,8 +4,14 @@ const router = Router();
 
 router.get('/', (req, res) => {
     res.render('home', {
-        title: 'Inicio'
-    })
+        title: 'Inicio',
+    });
+});
+
+router.get('/formulario-registro', (req, res) => {
+    res.render('register_form', {
+        title: 'Registro'
+    });
 });
 
 export default router;
